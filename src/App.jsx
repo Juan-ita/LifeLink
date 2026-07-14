05843cda922f884dbea87c0ba37b528455bf6c56
Juan-ita <kavatajuanita@gmail.com>
2026-07-14 14:31:18 +0300

    feat: fix routing and deploy project to vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Register from './pages/Register'
 
 function App() {
   return (
     <>
-    <Router>
         <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/register' element={<Register/>}/>
         </Routes>
-    </Router>
     
     </>
   )
