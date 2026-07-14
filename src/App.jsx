c3ae78544d982acbd2709f8b7e9d43732c92e325
Juan-ita <kavatajuanita@gmail.com>
2026-07-15 01:03:33 +0300

    feat: create new haspital dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import Hospitaldashboard from './pages/Hospitaldashboard'
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           <Route path='/' element={<Home/>}/>
           <Route path='/register' element={<Register/>}/>
           <Route path='/login' element={<Login/>}/>
-          <Route path='/hospitaldashboard' element={<Home/>}/>
+          <Route path='/hospitaldashboard' element={<Hospitaldashboard/>}/>
         </Routes>
     
     </>
---
+++ src/pages/Hospitaldashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Hospitaldashboard() {
+  return (
+    <div className='flex min-h-screen bg-gray-100'>
+      <main className='flex-1 p-8'>
+        <h1 className='text-3xl font-bold'>Hospital Dashboard</h1>
+        <p className='text-gray-500 mt-2'>Welcome Back</p>
+      </main>
+    </div>
+  )
+}
+
+export default Hospitaldashboard
