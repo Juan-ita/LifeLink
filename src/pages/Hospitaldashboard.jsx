47ecf70f61397bd2fb3a1a301ea69fc4be89a20a
Juan-ita <kavatajuanita@gmail.com>
2026-07-15 10:36:45 +0300

    feat: create new dashboard cards and add in hospital dashboard

--- src/pages/Hospitaldashboard.jsx
+++ src/pages/Hospitaldashboard.jsx
@@ -1,4 +1,7 @@
-import Sidebar from '@/hospital dashboard/Sidebar'
+import DashboardCard from '@/components/hospital/DashboardCard'
+import Sidebar from '@/components/hospital/Sidebar'
+import { Calendar, Droplets, ClipboardList, Users } from 'lucide-react'
+
 
 function Hospitaldashboard() {
   return (
@@ -6,7 +9,33 @@ function Hospitaldashboard() {
         <Sidebar/>
       <main className='flex-1 p-8'>
         <h1 className='text-3xl font-bold'>Hospital Dashboard</h1>
-        <p className='text-gray-500 mt-2'>Welcome Nairobi Hospital</p>
+        <p className='text-gray-500 mt-2'>Welcome back, Nairobi Hospital</p>
+
+        <div className='mt-8 grid md:grid-cols-2 xl:grid-cols-4 gap-6'>
+          <DashboardCard 
+          title="Registered Donors"
+          value="125"
+          icon={<Users size={28}/>}
+          />
+
+          <DashboardCard 
+          title="Blood Requests"
+          value="18"
+          icon={<ClipboardList size={28}/>}
+          />
+
+          <DashboardCard 
+          title="Blood Units"
+          value="46"
+          icon={<Droplets size={28}/>}
+          />
+
+          <DashboardCard 
+          title="Appointments"
+          value="12"
+          icon={<Calendar size={28}/>}
+          />
+        </div>
       </main>
     </div>
   )
