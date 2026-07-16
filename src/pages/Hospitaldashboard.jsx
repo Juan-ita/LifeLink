8379e6452cf64032c20143a0bf8edd42ced4c765
Juan-ita <kavatajuanita@gmail.com>
2026-07-17 00:35:03 +0300

    feat: make cards functional

--- src/pages/Hospitaldashboard.jsx
+++ src/pages/Hospitaldashboard.jsx
@@ -3,8 +3,61 @@ import Sidebar from '@/components/hospital/Sidebar'
 import { Calendar, Droplets, ClipboardList, Users } from 'lucide-react'
 import QuickActions from '@/components/hospital/QuickActions'
 import RecentRequests from '@/components/hospital/RecentRequests'
+import { useState, useEffect } from 'react'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '@/firebase/FirebaseConfig'
 
 function Hospitaldashboard() {
+
+  // Store dashboard statistics
+  const [status, setStatus] = useState ({
+    donors: 0,
+    requests: 0,
+    units: 0,
+    appointments: 0,
+  });
+
+  //Run once when the dashboard loads
+  useEffect(()=>{
+    async function fetchStatus (){
+      try{
+        //get all blood requests
+        const requestsSnapshot = await getDocs(
+          collection(db, "bloodRequests")
+        );
+        //Get all blood inventory
+        const inventorySnapshot = await getDocs(
+          collection(db, "bloodInventory")
+        );
+        //variable to store total blood units
+        let totalUnits = 0;
+
+        //loop through every inventory document
+        inventorySnapshot.forEach((doc) => {
+
+          //Add the units from each document
+          totalUnits += Number(doc.data().units|| 0);
+        })
+
+        //update the dashboard cards
+        setStatus({
+          donors: 0,
+
+          //Number of vlood requests
+          requests: requestsSnapshot.size,
+
+          //total blood units available
+          units: totalUnits,
+
+          appointments: 0,
+        })
+      } catch(error){
+        console.error(error);
+      }
+    }
+    //call the function
+    fetchStatus()
+  }, [])
   return (
     
     <div className='flex min-h-screen bg-gray-100'>
@@ -16,25 +69,25 @@ function Hospitaldashboard() {
         <div className='mt-8 grid md:grid-cols-2 xl:grid-cols-4 gap-6'>
           <DashboardCard 
           title="Registered Donors"
-          value="125"
+          value={status.donors}
           icon={<Users size={28}/>}
           />
 
           <DashboardCard 
           title="Blood Requests"
-          value="18"
+          value={status.requests}
           icon={<ClipboardList size={28}/>}
           />
 
           <DashboardCard 
           title="Blood Units"
-          value="46"
+          value={status.units}
           icon={<Droplets size={28}/>}
           />
 
           <DashboardCard 
           title="Appointments"
-          value="12"
+          value={status.appointments}
           icon={<Calendar size={28}/>}
           />
         </div>
