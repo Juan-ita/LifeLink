7eca4f2cc99bc21ff2c40c646b0c6ae2e79a5bb8
Juan-ita <kavatajuanita@gmail.com>
2026-07-16 10:12:08 +0300

    feat: create new inventory table

--- src/pages/BloodInventory.jsx
+++ src/pages/BloodInventory.jsx
@@ -6,6 +6,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
+import InventoryTable from '@/components/hospital/InventoryTable'
 
 function BloodInventory() {
   const [bloodGroup, setBloodGroup] = useState("")
@@ -96,6 +97,8 @@ function BloodInventory() {
           </form>
         </CardContent>
        </Card>
+
+       <InventoryTable/>
     </HospitalLayout>
   )
 }
