b74c717baebac7ad2dab94985e3d36bdc6924eaf
Juan-ita <kavatajuanita@gmail.com>
2026-07-18 11:03:52 +0300

    feat: add notification count

--- src/components/donor/DonorSidebar.jsx
+++ src/components/donor/DonorSidebar.jsx
@@ -1,7 +1,34 @@
 import { Link } from 'react-router-dom'
 import { LayoutDashboard, Droplets, Calendar, User, LogOut } from 'lucide-react'
+import { db, auth } from '@/firebase/FirebaseConfig'
+import { collection, getDocs } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
 
 function DonorSidebar() {
+
+  const [notificationCount, setNotificationCount] = useState(0)
+
+  useEffect (() => {
+    async function loadNotifications(){
+      //get all appointments
+      const querySnapshot = await getDocs(collection(db, "appointments")
+    );
+
+    //Keep only this donor's appointments
+    const myAppointments = querySnapshot.docs.filter((doc) => {
+      const data = doc.data();
+
+      return(
+        data.donorId === auth.currentUser.uid &&
+        data.status !== "Pending"
+      )
+    })
+
+    //Save notification count
+    setNotificationCount(myAppointments.length)
+    }
+    loadNotifications()
+  }, [])
   return (
     <aside className='w-64 min-h-screen bg-red-700 p-5 text-white'>
       <h2 className="mb-10 text-2xl font-bold">
@@ -9,22 +36,37 @@ function DonorSidebar() {
       </h2>
 
       <nav className='space-y-5'>
+        {/* Dashbosrd */}
         <Link to="/donor/dashboard"
         className='flex items-center gap-3 rounded p-3 hover:bg-red-800'>
             <LayoutDashboard/>
             Dashboard
         </Link>
 
+        {/* Blood requests */}
         <Link to="/donor/requests"
         className='flex items-center gap-3 rounded p-3 hover:bg-red-800'>
             <Droplets/>
             Blood Requests
         </Link>
+
+        {/* My Appointment */}
         <Link to="/donor/appointments"
         className='flex items-center gap-3 rounded p-3 hover:bg-red-800'>
-            <Calendar/>
-            My Appointments
+           <div className='flex items-center gap-3'>
+             <Calendar/>
+              <span>My Appointments</span>
+           </div>
+
+           {/* Notification badge */}
+           {notificationCount > 0 && (
+            <span className='rounded-full bg-white px-2 py-1 text-xs font-bold text-red-600'>
+              {notificationCount}
+            </span>
+           )}
         </Link>
+
+        {/* Profile */}
         <Link to="/donor/profile"
         className='flex items-center gap-3 rounded p-3 hover:bg-red-800'>
             <User/>
